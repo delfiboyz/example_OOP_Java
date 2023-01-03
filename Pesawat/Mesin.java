cc3b87a1db88cb83e10893bdf758a8918a48872f
delfiboyz <87855800+delfiboyz@users.noreply.github.com>
2023-01-04 04:06:24 +0700

    Update Mesin.java

--- Pesawat/Mesin.java
+++ Pesawat/Mesin.java
@@ -1,13 +1,39 @@
-public class Mesin{
-    double kecepatanMesin ;
-    int jarakTempuh ;
-    
-    void Mesin(double kecepatanMesin , int jarakTempuh){
-        this.kecepatanMesin = kecepatanMesin ;
-        this.jarakTempuh = jarakTempuh ;
-        System.out.println("kecepatan mesin adalah = " + kecepatanMesin+"/km ");
-        System.out.println("jarak tempuh adalah = " +jarakTempuh+ " km");
+interface MesinPesawat{
+    double setKecepatanMesin(double kecepatanMesin);
+    void getKecepatanMesin();
+    double setJarakTempuh(double jarakTempuh);
+    void getJarakTempuh();
+}
 
+public class Mesin implements MesinPesawat{
+    protected double kecepatanMesin ;
+    protected double jarakTempuh ;
+    @Override
+    public double setKecepatanMesin(double kecepatanMesin) {
+        // TODO Auto-generated method stub
+        this.kecepatanMesin = kecepatanMesin;
+        return kecepatanMesin;
+    }
+    @Override
+    public void getKecepatanMesin() {
+        // TODO Auto-generated method stub
+        System.out.println("kecepatan mesin : " + kecepatanMesin + " km/jam");
+        
+    }
+    @Override
+    public double setJarakTempuh(double jarakTempuh) {
+        // TODO Auto-generated method stub
+        this.jarakTempuh = jarakTempuh ;
+        return jarakTempuh;
+    }
+    @Override
+    public void getJarakTempuh() {
+        // TODO Auto-generated method stub
+        System.out.println("jarak tempuh : " + jarakTempuh + " km");
+        
     }
 
+
+    
+    
 }
