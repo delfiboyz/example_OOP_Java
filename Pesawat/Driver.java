1d2d4b426b4e9ee7c93e9100f19a823d7954a6a3
delfiboyz <87855800+delfiboyz@users.noreply.github.com>
2023-01-08 23:22:47 +0700

    Update Driver.java

--- Pesawat/Driver.java
+++ Pesawat/Driver.java
@@ -3,6 +3,12 @@ public class Driver {
 
         PesawatTesla p1 = new PesawatTesla();
         p1.info();
+        System.out.println("===============================");
+        PesawatJet p2 = new PesawatJet();
+        p2.info();
+        System.out.println("===============================");
+        PesawatTempur p3 = new PesawatTempur();
+        p3.info();
         
 
 
